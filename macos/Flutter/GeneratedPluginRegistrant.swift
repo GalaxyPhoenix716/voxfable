1c000dc536d8379e053bcfd95e341b3b7add08e4
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-06-19 15:38:01 +0530

    initialized project structure and uploaded poppins font

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import flutter_tts
+import rive_common
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
+}
