2008e6a4f61e621a7e598dabbdfe34553ed577fd
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-06-21 16:25:32 +0530

    creating a caching service for the audio recieved from elevenlabs

---
+++ lib/core/audio/audio_cache_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:crypto/crypto.dart';
+import 'package:path_provider/path_provider.dart';
+
+class AudioCacheService {
+  Future<String> _getCacheKey(String text) async {
+    final bytes = utf8.encode(text);
+    final digest = sha256.convert(bytes);
+    final tempDir = await getTemporaryDirectory();
+
+    return '${tempDir.path}/tts_${digest.toString()}.mp3';
+  }
+
+  Future<File?> getCachedAudio(String text) async {
+    final path = await _getCacheKey(text);
+    final file = File(path);
+    if(await file.exists()) {
+      return file;
+    }
+
+    return null;
+  }
+}
