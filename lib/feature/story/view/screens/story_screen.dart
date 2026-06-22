b9ed9691e769075b637a50d389aa7b6b7610fb9f
GalaxyPhoenix <galaxyphoenix716@gmail.com>
2026-06-22 11:30:11 +0530

    added assets for bg, modified quiz answers, created emtpy story screen and cleaned main.dart

---
+++ lib/feature/story/view/screens/story_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class StoryScreen extends StatefulWidget {
+  const StoryScreen({super.key});
+
+  @override
+  State<StoryScreen> createState() => _StoryScreenState();
+}
+
+class _StoryScreenState extends State<StoryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [
+      ],));
+  }
+}
